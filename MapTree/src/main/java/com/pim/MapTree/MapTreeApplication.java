c67e1a306120376bbf740ae0c961a258d8713fb3
raissanana <raissalimmaa@gmail.com>
2026-02-23 14:42:32 -0300

    first commit

---
+++ MapTree/src/main/java/com/pim/MapTree/MapTreeApplication.java
@@ -0,0 +1,13 @@
+package com.pim.MapTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapTreeApplication.class, args);
+	}
+
+}
---
+++ MapTree/src/test/java/com/pim/MapTree/MapTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pim.MapTree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
